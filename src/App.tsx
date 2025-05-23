4b7ce583be4135502918f3a908f9e816e2a009a4
Alfred Lewis <alewis1719@gmail.com>
2025-05-23 18:28:05 +0100

    Add basic query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { routes } from "@/routes";
+import { queryClient } from "@/lib/api-client";
 
 const router = createBrowserRouter(routes);
 
 const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {import.meta.env.VITE_HIDE_QUERY_TOOLS !== "true" && (
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-right"
+        />
+      )}
+    </QueryClientProvider>
+  );
 };
 
 export default App;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,11 @@
-const HomePage = () => {
-  return <div>Hi world</div>;
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold">Home Page</h1>
+    </div>
+  );
 };
 
 export default HomePage;
