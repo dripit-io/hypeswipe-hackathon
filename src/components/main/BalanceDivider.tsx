b492ee6e8e0b43af6db4d64b7db614a368862599
Alfred Lewis <alewis1719@gmail.com>
2025-05-24 12:38:26 +0100

    Add custom fonts

--- src/components/main/BalanceDivider.tsx
+++ src/components/main/BalanceDivider.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { TrophyIcon } from "lucide-react";
 
-const arenaBalance = 200;
-const claimAmount = 50.57;
+interface BalanceDividerProps {
+  arenaBalance: number;
+  claimAmount: number;
+}
 
-export const BalanceDivider: React.FC = () => (
+export const BalanceDivider: React.FC<BalanceDividerProps> = ({
+  arenaBalance,
+  claimAmount,
+}) => (
   <div className="flex flex-col items-center w-full px-4">
     <div className="flex items-center w-full gap-2">
       <div className="h-[1px] flex-grow bg-[#203F3B]" />
@@ -15,13 +20,19 @@ export const BalanceDivider: React.FC = () => (
             alt="arena logo"
             className="size-4"
           />
-          <p className="text-[#0F0915] font-['TT_Fors_Trial'] text-base font-bold leading-normal">{arenaBalance}</p>
-          <span className="text-[#0F0915] font-['TT_Fors_Trial'] text-sm font-medium leading-normal">$ARENA</span>
+          <p className="text-[#0F0915] text-base font-bold leading-normal">
+            {arenaBalance}
+          </p>
+          <span className="text-[#0F0915] text-sm font-medium leading-normal">
+            $ARENA
+          </span>
         </p>
         {claimAmount > 0 && (
           <p className="text-sm px-2 py-1 flex gap-1.5 items-center rounded-lg bg-white/12 text-white">
             <TrophyIcon className="size-3 text-white" />
-            <p className="text-white font-['TT_Fors_Trial'] text-base font-bold leading-normal">{claimAmount}</p>
+            <p className="text-white text-base font-bold leading-normal">
+              {claimAmount}
+            </p>
           </p>
         )}
       </div>
